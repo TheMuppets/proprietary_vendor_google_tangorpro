260ec2bdbdc31b1e29b4a52d046fa3f2e7a404b4
Michael Bestas <mkbestas@lineageos.org>
2024-09-08 02:31:11 +0300

    tangorpro: Add xml suffix to prebuilt_etc_xml modules
    Change-Id: I294228e622a9965fa38bd93c9481a0547c520e82

--- tangorpro-vendor.mk
+++ tangorpro-vendor.mk
@@ -74,7 +74,7 @@ PRODUCT_PACKAGES += \
     com.google.android.camera.extensions \
     com.google.android.camerax.extensions \
     google-ril \
-    vendor.google.edgetpu_app_service@1.0-service \
+    vendor.google.edgetpu_app_service@1.0-service.xml \
     vendor.google.edgetpu_app_service@1.0-service
 
 PRODUCT_PACKAGES += \
@@ -350,26 +350,26 @@ PRODUCT_PACKAGES += \
     com.google.pixel.camera.hal \
     com.google.pixel.wifi.ext \
     com.google.android.camera.experimental2022 \
-    android.hardware.authsecret-service.citadel \
-    android.hardware.camera.provider@2.7-service-google-apex \
-    android.hardware.confirmationui-service.trusty.vendor \
-    android.hardware.contexthub-service.generic \
-    android.hardware.identity.strongbox \
-    android.hardware.neuralnetworks@service-darwinn-aidl \
-    android.hardware.oemlock-service.citadel \
-    android.hardware.security.keymint-service.citadel \
-    android.hardware.weaver-service.citadel \
-    com.google.edgetpu.dba-service \
-    com.google.edgetpu.tachyon-service \
-    fingerprint-fpc42 \
-    manifest_input.processor-service \
-    manifest_radioext \
-    manifest_wifi_ext_aidl \
-    qorvo.uwb-service \
-    vendor.dolby.media.c2@1.0-service \
-    vendor.google.battery_mitigation-default \
-    vendor.google.edgetpu_vendor_service@1.0-service \
-    vendor.google.google_battery-default \
+    android.hardware.authsecret-service.citadel.xml \
+    android.hardware.camera.provider@2.7-service-google-apex.xml \
+    android.hardware.confirmationui-service.trusty.vendor.xml \
+    android.hardware.contexthub-service.generic.xml \
+    android.hardware.identity.strongbox.xml \
+    android.hardware.neuralnetworks@service-darwinn-aidl.xml \
+    android.hardware.oemlock-service.citadel.xml \
+    android.hardware.security.keymint-service.citadel.xml \
+    android.hardware.weaver-service.citadel.xml \
+    com.google.edgetpu.dba-service.xml \
+    com.google.edgetpu.tachyon-service.xml \
+    fingerprint-fpc42.xml \
+    manifest_input.processor-service.xml \
+    manifest_radioext.xml \
+    manifest_wifi_ext_aidl.xml \
+    qorvo.uwb-service.xml \
+    vendor.dolby.media.c2@1.0-service.xml \
+    vendor.google.battery_mitigation-default.xml \
+    vendor.google.edgetpu_vendor_service@1.0-service.xml \
+    vendor.google.google_battery-default.xml \
     aocd \
     aocxd \
     android.hardware.authsecret-service.citadel \
