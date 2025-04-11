4b2c50ff095ea4a13ef6f52fa80106916e2049d3
Michael Bestas <mkbestas@lineageos.org>
2025-04-11 20:59:49 +0300

    tangorpro: Import blobs & firmware from BP1A.250405.007
    Change-Id: Icabcb5370c569cfe3e9aba195ddd12414c8a73a3

--- Android.mk
+++ Android.mk
@@ -6,13 +6,13 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),tangorpro)
 
-$(call add-radio-file-sha1-checked,radio/abl.img,18032dbe757c05b462c963780aeefd537a2931ed)
-$(call add-radio-file-sha1-checked,radio/bl1.img,eb05699adebab01537808a2be6a105c24b0c3bf5)
-$(call add-radio-file-sha1-checked,radio/bl2.img,d5e0b226e1e513da4f26abe7be0f0fd5bf949ce8)
-$(call add-radio-file-sha1-checked,radio/bl31.img,6c76c0eeeb3f7bf1836af173e77e9f4c53d2f704)
-$(call add-radio-file-sha1-checked,radio/gsa.img,e5ef76ecf2af85b2c231b749321fed87aa500f5e)
-$(call add-radio-file-sha1-checked,radio/ldfw.img,e847e7b5c8e90eb554e0c406098f26f9a81dfd5a)
-$(call add-radio-file-sha1-checked,radio/pbl.img,be4f5202baf8cf2a262575d0391bad6a2d5c76c2)
-$(call add-radio-file-sha1-checked,radio/tzsw.img,4a0353ef768cbf0a62462cb9b7b8ade4f1e3d4fd)
+$(call add-radio-file-sha1-checked,radio/abl.img,7dbeb029dfb539a9bc213e731698ecbf3c445a06)
+$(call add-radio-file-sha1-checked,radio/bl1.img,13db63f81cce931274d8e37d0bd4762c36a9a768)
+$(call add-radio-file-sha1-checked,radio/bl2.img,e2e53d1a2525d751d9b659eab4c3453f0919a49b)
+$(call add-radio-file-sha1-checked,radio/bl31.img,11c9c4f0ec1ce0047480488a37e2769c8a05a58a)
+$(call add-radio-file-sha1-checked,radio/gsa.img,8d869258d71e01078202a7afcf29632c2709663e)
+$(call add-radio-file-sha1-checked,radio/ldfw.img,bf82a220de24d1914183e71fbed0a315f1e28ddb)
+$(call add-radio-file-sha1-checked,radio/pbl.img,72057970938ee424a70e45dd5db5e5fed6d22ef3)
+$(call add-radio-file-sha1-checked,radio/tzsw.img,125bc3b32eaf23676bcafa4129268df575976da1)
 
 endif
